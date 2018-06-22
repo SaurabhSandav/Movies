8bd66360884f44827e00466f506b40cf0ed97c6c
Saurabh Sandav <justmailsaurabh@gmail.com>
2018-06-22 12:40:01 +0530

    Main: Setup Injection

--- app/src/main/java/com/redridgeapps/movies/di/modules/ActivityBuilder.java
+++ app/src/main/java/com/redridgeapps/movies/di/modules/ActivityBuilder.java
@@ -1,8 +1,15 @@
 package com.redridgeapps.movies.di.modules;
 
+import com.redridgeapps.movies.di.scopes.PerActivity;
+import com.redridgeapps.movies.screen.main.MainActivity;
+
 import dagger.Module;
+import dagger.android.ContributesAndroidInjector;
 
 @Module
 public abstract class ActivityBuilder {
 
+    @PerActivity
+    @ContributesAndroidInjector()
+    public abstract MainActivity bindMainActivity();
 }
