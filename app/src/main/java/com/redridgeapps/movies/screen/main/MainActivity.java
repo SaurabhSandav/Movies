663375603511c1685665c3ad622cd48cb47b3188
Saurabh Sandav <justmailsaurabh@gmail.com>
2018-06-22 13:30:53 +0530

    Add BaseActivity

--- app/src/main/java/com/redridgeapps/movies/screen/main/MainActivity.java
+++ app/src/main/java/com/redridgeapps/movies/screen/main/MainActivity.java
@@ -1,15 +1,20 @@
 package com.redridgeapps.movies.screen.main;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import com.redridgeapps.movies.R;
+import com.redridgeapps.movies.screen.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity<MainViewModel> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected Class<MainViewModel> provideViewModelClass() {
+        return MainViewModel.class;
     }
 }
