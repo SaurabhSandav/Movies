d4416c383b35b675a88da080559bdaad0884a81e
Saurabh Sandav <justmailsaurabh@gmail.com>
2018-06-27 20:10:51 +0530

    DetailScreen: Display data

--- app/src/main/java/com/redridgeapps/movies/screen/detail/DetailActivity.java
+++ app/src/main/java/com/redridgeapps/movies/screen/detail/DetailActivity.java
@@ -3,6 +3,7 @@ package com.redridgeapps.movies.screen.detail;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ViewGroup;
 
 import com.redridgeapps.movies.R;
 import com.redridgeapps.movies.databinding.ActivityDetailBinding;
@@ -26,6 +27,7 @@ public class DetailActivity extends BaseActivity<DetailViewModel, ActivityDetail
         super.onCreate(savedInstanceState);
 
         Movie movie = getIntent().getParcelableExtra(EXTRA_MOVIE);
+        setupLayout(movie);
     }
 
     @Override
@@ -36,5 +38,36 @@ public class DetailActivity extends BaseActivity<DetailViewModel, ActivityDetail
     @Override
     protected Class<DetailViewModel> provideViewModelClass() {
         return DetailViewModel.class;
+    }
+
+    private void setupLayout(Movie movie) {
+
+        setSupportActionBar(getBinding().toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        getBinding().collapsingToolbar.setTitle(movie.getTitle());
+        getBinding().collapsingToolbar.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
+
+        // Set backdrop aspect ratio to 16:9
+        int fullWidth = getResources().getDisplayMetrics().widthPixels;
+        ViewGroup.LayoutParams layoutParams = getBinding().movieBackdrop.getLayoutParams();
+        layoutParams.width = fullWidth;
+        layoutParams.height = (int) (fullWidth * 0.56);
+
+        if (getBinding().mainDetail != null) {
+
+            // Calculate poster size to be relative to screen size with 3:2 aspect ratio.
+            float presetItemWidth = getResources().getDimension(R.dimen.default_movie_poster_width);
+            int columns = (int) Math.ceil(fullWidth / presetItemWidth);
+            int itemWidth = fullWidth / columns;
+
+            getBinding().mainDetail.moviePoster.getLayoutParams().width = itemWidth;
+            getBinding().mainDetail.moviePoster.getLayoutParams().height = (int) (itemWidth * 1.5);
+        }
+
+        getBinding().setMovie(movie);
     }
 }
