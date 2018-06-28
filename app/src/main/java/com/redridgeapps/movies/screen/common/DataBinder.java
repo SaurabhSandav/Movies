de4643bef701671120a582164ec17279589d35d8
Saurabh Sandav <justmailsaurabh@gmail.com>
2018-06-28 12:47:20 +0530

    Add placeholder and error drawables for Glide

--- app/src/main/java/com/redridgeapps/movies/screen/common/DataBinder.java
+++ app/src/main/java/com/redridgeapps/movies/screen/common/DataBinder.java
@@ -1,23 +1,30 @@
 package com.redridgeapps.movies.screen.common;
 
 import android.databinding.BindingAdapter;
+import android.graphics.drawable.Drawable;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.redridgeapps.movies.util.GlideApp;
 
 public final class DataBinder {
 
     private DataBinder() {
     }
 
-    @BindingAdapter("imageUrl")
-    public static void setImageUrl(ImageView imageView, String url) {
-        // TODO handle placeholder, error, null
+    @BindingAdapter({"imageUrl", "placeholder", "error"})
+    public static void setImageUrl(
+            ImageView imageView, String url,
+            Drawable placeholder,
+            Drawable error
+    ) {
+
         if (url != null) {
-            Glide.with(imageView.getContext())
+            GlideApp.with(imageView.getContext())
                     .load(url)
-                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .fitCenter()
+                    .placeholder(placeholder)
+                    .error(error)
                     .into(imageView);
         } else {
             Glide.with(imageView.getContext()).clear(imageView);
